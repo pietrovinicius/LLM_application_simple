aef0b49ed0e8bd664f25379e8e76495d3d7f8b3b
Pietro Lima <pietrovinicius@hotmail.com>
2025-07-15 14:42:35 -0300

    inicio

---
+++ LLM_application_simple.py
@@ -0,0 +1,15 @@
+#04/10/2024
+#@PLima
+
+#https://python.langchain.com/docs/tutorials/llm_chain/
+
+#Crie um aplicativo LLM simples com modelos de bate-papo e modelos de prompt
+
+#Neste guia rápido, mostraremos como criar um aplicativo LLM simples com o LangChain. 
+#Este aplicativo traduzirá textos do inglês para outro idioma. 
+#Trata-se de um aplicativo LLM relativamente simples: 
+# consiste em apenas uma chamada de LLM e alguns prompts. 
+# 
+#Ainda assim, é uma ótima maneira de começar a usar o LangChain: 
+#muitos recursos podem ser criados com apenas alguns prompts e uma chamada de LLM!
+
